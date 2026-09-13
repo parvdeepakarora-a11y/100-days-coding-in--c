d49f957502d60bb038a0ac9c9000003475e180ab
parvdeepakarora-a11y <parvdeepakarora@gmail.com>
2026-09-14 02:11:16 +0530

    Day9 Q1 sol

---
+++ Day9 Question1.c
@@ -0,0 +1,36 @@
+
+   #include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, D, root1, root2;
+
+    printf("Enter a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    D = b * b - 4 * a * c;
+
+    if (D > 0)
+    {
+        root1 = (-b + sqrt(D)) / (2 * a);
+        root2 = (-b - sqrt(D)) / (2 * a);
+
+        printf("Roots are real and different.\n");
+        printf("Root 1 = %.2f\n", root1);
+        printf("Root 2 = %.2f\n", root2);
+    }
+    else if (D == 0)
+    {
+        root1 = -b / (2 * a);
+
+        printf("Roots are real and equal.\n");
+        printf("Root = %.2f\n", root1);
+    }
+    else
+    {
+        printf("Roots are imaginary (complex).\n");
+    }
+
+    return 0;
+}
