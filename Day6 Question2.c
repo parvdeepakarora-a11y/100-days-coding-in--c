117fb8fc5b85692db9b6278bf13d5022e512521a
parvdeepakarora-a11y <parvdeepakarora@gmail.com>
2026-09-14 02:07:46 +0530

    Day6 Q2 sol

---
+++ Day6 Question2.c
@@ -0,0 +1,13 @@
+ 
+#include <stdio.h>
+int main()
+{int n;
+printf("enter a number= ");
+scanf("%d",&n);
+if(n>0)
+{printf("positive\n");}
+else if(n<0)
+{printf("negative\n");}
+else
+{printf("zero\n");}
+return 0;}
