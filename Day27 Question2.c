013a755d23061ea6d6797252f7bb5835bf36d94e
parvdeepakarora-a11y <parvdeepakarora@gmail.com>
2026-09-17 23:55:49 +0530

    Day27 Question2.c

---
+++ Day27 Question2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, spaces, stars;
+
+    // Upper half
+    for (i = 1; i <= 4; i++) {
+        spaces = 4 - i;
+        stars = 2 * i - 1;
+
+        for (j = 1; j <= spaces; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= stars; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    // Lower half
+    for (i = 3; i >= 1; i--) {
+        spaces = 4 - i;
+        stars = 2 * i - 1;
+
+        for (j = 1; j <= spaces; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= stars; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
